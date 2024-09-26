dac4ed444651867d434fafb7bd162c71e160f161
spoorthiuk <spoorthiuday27@gmail.com>
2024-09-25 21:52:29 -0700

    Initial Commit

---
+++ SpokenBudget/SpokenBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpokenBudgetApp.swift
+//  SpokenBudget
+//
+//  Created by Spoorthi U K on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SpokenBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
